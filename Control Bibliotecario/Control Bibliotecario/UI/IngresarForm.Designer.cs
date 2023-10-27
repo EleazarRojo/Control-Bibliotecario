c259f6e7076b52650d4f697ddaa89517fd4537b1
cesar <cesar@DESKTOP-KNJP8RE>
2023-10-26 19:25:46 -0700

    pull cesar

--- Control Bibliotecario/Control Bibliotecario/UI/IngresarForm.Designer.cs
+++ Control Bibliotecario/Control Bibliotecario/UI/IngresarForm.Designer.cs
@@ -29,27 +29,31 @@ namespace Control_Bibliotecario
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.idUsuario_tbx = new System.Windows.Forms.TextBox();
             this.idUsario_lbl = new System.Windows.Forms.Label();
             this.Titulo_lbl = new System.Windows.Forms.Label();
-            this.contraseña_lbl = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.acceder_btn = new System.Windows.Forms.Button();
+            this.nombre_lbl = new System.Windows.Forms.Label();
+            this.apellidoPaterno_lbl = new System.Windows.Forms.Label();
+            this.apellidoMaterno_lbl = new System.Windows.Forms.Label();
+            this.nombre_tbx = new System.Windows.Forms.TextBox();
+            this.apellidoPaterno_tbx = new System.Windows.Forms.TextBox();
+            this.apellidoMaterno_tbx = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // textBox1
+            // idUsuario_tbx
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(169, 124);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(128, 26);
-            this.textBox1.TabIndex = 5;
+            this.idUsuario_tbx.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.idUsuario_tbx.Location = new System.Drawing.Point(141, 90);
+            this.idUsuario_tbx.Name = "idUsuario_tbx";
+            this.idUsuario_tbx.Size = new System.Drawing.Size(128, 26);
+            this.idUsuario_tbx.TabIndex = 5;
             // 
             // idUsario_lbl
             // 
             this.idUsario_lbl.AutoSize = true;
             this.idUsario_lbl.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.idUsario_lbl.Location = new System.Drawing.Point(192, 101);
+            this.idUsario_lbl.Location = new System.Drawing.Point(54, 94);
             this.idUsario_lbl.Name = "idUsario_lbl";
             this.idUsario_lbl.Size = new System.Drawing.Size(76, 19);
             this.idUsario_lbl.TabIndex = 4;
@@ -59,29 +63,11 @@ namespace Control_Bibliotecario
             // 
             this.Titulo_lbl.AutoSize = true;
             this.Titulo_lbl.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Titulo_lbl.Location = new System.Drawing.Point(148, 39);
+            this.Titulo_lbl.Location = new System.Drawing.Point(148, 22);
             this.Titulo_lbl.Name = "Titulo_lbl";
             this.Titulo_lbl.Size = new System.Drawing.Size(175, 31);
             this.Titulo_lbl.TabIndex = 3;
             this.Titulo_lbl.Text = "Member Login";
-            // 
-            // contraseña_lbl
-            // 
-            this.contraseña_lbl.AutoSize = true;
-            this.contraseña_lbl.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.contraseña_lbl.Location = new System.Drawing.Point(187, 171);
-            this.contraseña_lbl.Name = "contraseña_lbl";
-            this.contraseña_lbl.Size = new System.Drawing.Size(76, 19);
-            this.contraseña_lbl.TabIndex = 6;
-            this.contraseña_lbl.Text = "Contaseña:";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(169, 194);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(128, 26);
-            this.textBox2.TabIndex = 7;
             // 
             // acceder_btn
             // 
@@ -92,20 +78,80 @@ namespace Control_Bibliotecario
             this.acceder_btn.TabIndex = 8;
             this.acceder_btn.Text = "Acceder";
             this.acceder_btn.UseVisualStyleBackColor = true;
+            this.acceder_btn.Click += new System.EventHandler(this.acceder_btn_Click);
+            // 
+            // nombre_lbl
+            // 
+            this.nombre_lbl.AutoSize = true;
+            this.nombre_lbl.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nombre_lbl.Location = new System.Drawing.Point(67, 143);
+            this.nombre_lbl.Name = "nombre_lbl";
+            this.nombre_lbl.Size = new System.Drawing.Size(63, 19);
+            this.nombre_lbl.TabIndex = 9;
+            this.nombre_lbl.Text = "Nombre:";
+            // 
+            // apellidoPaterno_lbl
+            // 
+            this.apellidoPaterno_lbl.AutoSize = true;
+            this.apellidoPaterno_lbl.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.apellidoPaterno_lbl.Location = new System.Drawing.Point(16, 173);
+            this.apellidoPaterno_lbl.Name = "apellidoPaterno_lbl";
+            this.apellidoPaterno_lbl.Size = new System.Drawing.Size(114, 19);
+            this.apellidoPaterno_lbl.TabIndex = 10;
+            this.apellidoPaterno_lbl.Text = "Apellido Paterno:";
+            // 
+            // apellidoMaterno_lbl
+            // 
+            this.apellidoMaterno_lbl.AutoSize = true;
+            this.apellidoMaterno_lbl.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.apellidoMaterno_lbl.Location = new System.Drawing.Point(16, 209);
+            this.apellidoMaterno_lbl.Name = "apellidoMaterno_lbl";
+            this.apellidoMaterno_lbl.Size = new System.Drawing.Size(119, 19);
+            this.apellidoMaterno_lbl.TabIndex = 11;
+            this.apellidoMaterno_lbl.Text = "Apellido Materno:";
+            // 
+            // nombre_tbx
+            // 
+            this.nombre_tbx.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nombre_tbx.Location = new System.Drawing.Point(141, 136);
+            this.nombre_tbx.Name = "nombre_tbx";
+            this.nombre_tbx.Size = new System.Drawing.Size(128, 26);
+            this.nombre_tbx.TabIndex = 12;
+            // 
+            // apellidoPaterno_tbx
+            // 
+            this.apellidoPaterno_tbx.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.apellidoPaterno_tbx.Location = new System.Drawing.Point(141, 168);
+            this.apellidoPaterno_tbx.Name = "apellidoPaterno_tbx";
+            this.apellidoPaterno_tbx.Size = new System.Drawing.Size(128, 26);
+            this.apellidoPaterno_tbx.TabIndex = 13;
+            // 
+            // apellidoMaterno_tbx
+            // 
+            this.apellidoMaterno_tbx.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.apellidoMaterno_tbx.Location = new System.Drawing.Point(141, 202);
+            this.apellidoMaterno_tbx.Name = "apellidoMaterno_tbx";
+            this.apellidoMaterno_tbx.Size = new System.Drawing.Size(128, 26);
+            this.apellidoMaterno_tbx.TabIndex = 14;
             // 
             // IngresarForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(518, 322);
+            this.Controls.Add(this.apellidoMaterno_tbx);
+            this.Controls.Add(this.apellidoPaterno_tbx);
+            this.Controls.Add(this.nombre_tbx);
+            this.Controls.Add(this.apellidoMaterno_lbl);
+            this.Controls.Add(this.apellidoPaterno_lbl);
+            this.Controls.Add(this.nombre_lbl);
             this.Controls.Add(this.acceder_btn);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.contraseña_lbl);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.idUsuario_tbx);
             this.Controls.Add(this.idUsario_lbl);
             this.Controls.Add(this.Titulo_lbl);
             this.Name = "IngresarForm";
             this.Text = "Ingresar";
+            this.Load += new System.EventHandler(this.IngresarForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -113,11 +159,15 @@ namespace Control_Bibliotecario
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox idUsuario_tbx;
         private System.Windows.Forms.Label idUsario_lbl;
         private System.Windows.Forms.Label Titulo_lbl;
-        private System.Windows.Forms.Label contraseña_lbl;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button acceder_btn;
+        private System.Windows.Forms.Label nombre_lbl;
+        private System.Windows.Forms.Label apellidoPaterno_lbl;
+        private System.Windows.Forms.Label apellidoMaterno_lbl;
+        private System.Windows.Forms.TextBox nombre_tbx;
+        private System.Windows.Forms.TextBox apellidoPaterno_tbx;
+        private System.Windows.Forms.TextBox apellidoMaterno_tbx;
     }
 }
